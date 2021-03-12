1ccc6e921dcab6889137b82cc82d7a968a3d5d07
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-12 05:21:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function wakeDog(dogName, dogBreed) {
-  return `Wake ${dogName} the ${dogBreed}`;
+  console.log (`Wake ${dogName} the ${dogBreed}`);
 }
 
 function leashDog(dogName, dogBreed) {
-  return 'Leash ${dogName} the ${dogBreed}';
+  console.log (`Leash ${dogName} the ${dogBreed}`);
 }
 
 function walkToPark(dogName, dogBreed) {
-  return 'Walk to the park with ${dogName} the ${dogBreed}';
+  console.log (`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  return 'Throw the frisbee for ${dogName} the ${dogBreed}';
+  console.log (`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
 function walkHome(dogName, dogBreed) {
-  return 'Walk home with ${dogName} the ${dogBreed}';
+  console.log (`Walk home with ${dogName} the ${dogBreed}`);
 }
 
 function unleashDog(dogName, dogBreed) {
-  return 'Unleash ${dogName} the ${dogBreed}';
+  console.log (`Unleash ${dogName} the ${dogBreed}`);
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
